604f059486cec0ef7b707fce40c4ceb7f45d6b8f
Prarthana G <prarthanajg@live.com>
2022-04-13 13:43:06 -0400

    SQL querying from external file works

--- backend/MySQL_JDBC.java
+++ backend/MySQL_JDBC.java
@@ -1,22 +1,29 @@
-import java.io.FileReader;
+import java.io.*;
 import java.sql.*;
 import java.util.*;
-// import java.io.*;
-// import org.apache.ibatis.jdbc.ScriptRunner;
+import java.lang.*;
+import org.apache.ibatis.jdbc.ScriptRunner;
 public class MySQL_JDBC {
 
    static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/?user=projectbasedorgs";
    static final String USER = "projectbasedorgs";
    static final String PASS = "Pass!23";
 
-   public static void main(String[] args) {
+   /**
+    * stuff to do that prarthana doesn't wanna deal with right now:
+    *  - better exception management
+    * @param args
+    * @throws Exception
+    */
+   public static void main(String[] args) throws Exception{
       // Open a connection
       try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
          Statement stmt = conn.createStatement();
       ) {		      
-         String sql = "CREATE DATABASE STUDENTS";
-         stmt.executeUpdate(sql);
-         System.out.println("Database created successfully...");   	  
+         ScriptRunner sr = new ScriptRunner(conn);
+         Reader reader = new BufferedReader(new FileReader("backend/sql_files/tables.sql"));
+         sr.runScript(reader);
+         System.out.println("Executed successfully");   	  
       } catch (SQLException e) {
          e.printStackTrace();
       } 
--- backend/sql_files/tables.sql
+++ backend/sql_files/tables.sql
@@ -1,1 +1,11 @@
-SELECT * FROM COMPANY 
+CREATE DATABASE testDB;
+CREATE TABLE testDB.Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Addr varchar(255),
+    City varchar(255)
+);
+
+INSERT INTO testDB.Persons (PersonID, LastName, FirstName, Addr, City)
+VALUE ('1', 'Gajjala', 'Prarthana', '123 Main Street', 'Cleveland');
