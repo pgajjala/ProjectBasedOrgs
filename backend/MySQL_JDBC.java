142d30729540403072303dc61d2cec6a0e8c47ee
Prarthana G <prarthanajg@live.com>
2022-04-15 11:19:49 -0400

    changed db_url

--- backend/MySQL_JDBC.java
+++ backend/MySQL_JDBC.java
@@ -5,7 +5,7 @@ import java.lang.*;
 import org.apache.ibatis.jdbc.ScriptRunner;
 public class MySQL_JDBC {
 
-   static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/";
+   static String DB_URL = "";
    static final String USER = "projectbasedorgs";
    static final String PASS = "Pass!23";
 
@@ -16,6 +16,8 @@ public class MySQL_JDBC {
     * @throws Exception
     */
    public static void main(String[] args) throws Exception{
+      Scanner fr = new Scanner(new File("untracked.txt"));
+      DB_URL = fr.nextLine();
       // Open a connection
       try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
          Statement stmt = conn.createStatement();
