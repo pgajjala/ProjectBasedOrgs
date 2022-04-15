a30722943e71cf15034300815b3c1d1e48341bc2
Nivita <nnp25@case.edu>
2022-04-15 19:57:28 -0400

    changed query 3, query 6

--- backend/sql_files/query6.sql
+++ backend/sql_files/query6.sql
@@ -1,7 +1,7 @@
-Select p.project_title, u.state
-From Project p, University u, Works_on w, Registered r, Project_Based_Club c
-Where u.university_name = r.university_name
+SELECT p.project_title, u.state
+FROM Project p, University u, Works_on w, Registered r, Project_Based_Club c
+WHERE u.university_name = r.university_name
 AND r.club_name = c.club_name
 AND c.club_name = w.club_name
 AND w.project_title = p.project_title
-Group by u.state
+GROUP BY u.state
